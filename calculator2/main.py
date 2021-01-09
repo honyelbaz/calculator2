993c115023c0d9d4cd4172955e3ed3c1501e0e9e
honyelbaz <76423641+honyelbaz@users.noreply.github.com>
2021-01-09 22:06:08 +0200

    Add files via upload

--- calculator2/main.py
+++ calculator2/main.py
@@ -1,18 +1,30 @@
-"""todo -
--handle the case of 23e+4e+3
+import solver
+import input_output as io
 
-
-"""
 
 #get input from user
-
+eq = io.get_equation()
+res = None
 #while input != "stop"
-
+while eq.lower() != "stop":
     #solve the input
+    try:
+        res = solver.solve(eq)
+    except Exception as e:
+        #print reason why it's unsolvable
+        if e.args[0] == "maximum recursion depth exceeded in comparison":
+            print("the result is too large for calculating")
+        else:
+            print(e)
 
     #if solved
+    if res is not None:
+        if res == "inf":
+            print("the result is too large for calculating")
         #print the result
-    #if not solved
-        #print reason why it's unsolvable
+        else:
+            print(res)
+    res = None
 
     #get input from user
+    eq = io.get_equation()
--- calculator2/solver.py
+++ calculator2/solver.py
@@ -1,6 +1,47 @@
+import validator
+import solver_helper
+import finder
 
 
-def solve(eq, i):
-    #get rid of all the brackets expressions with recursive
+#gets a list and makes all the operations
+def clear_from_operators(lst):
+    i = finder.find_strongest_operator(lst)
+    while i != -1:
+        if finder.is_operator(lst[i]):
+            lst = solver_helper.list_make_operation(lst, i)
+        i = finder.find_strongest_operator(lst)
+    return lst
 
-    pass
+
+# gets an expression and returns the result of it
+def solve(eq):
+    if not validator.is_valid(eq):
+        return None
+    #make haircut for the minuses
+    eq = solver_helper.minuses_haircut(eq)
+    #strip the expression from it's brackets if necessary
+    if solver_helper.needs_to_be_bracket_striped(eq):
+        eq = solver_helper.strip_outer_brackets(eq)
+        if solver_helper.needs_to_be_bracket_striped(eq):
+            raise Exception("unnecessary brackets on an expression: (" + str(eq) + ")")
+    #make a list
+    lst = solver_helper.make_a_list(eq)
+    #(on the list)
+
+    #if there are expressions (expression is a)
+    i = finder.find_expression(lst)
+    while i != -1:
+        res = solve(lst[i])
+        lst[i] = res
+        i = finder.find_expression(lst)
+
+    if solver_helper.list_is_valid(lst):
+        pass
+    #while len(lst) > 1 or lst[0] is not an expression
+        #find the strongest operator and operate
+    lst = clear_from_operators(lst)
+    if solver_helper.list_is_valid(lst):
+        pass
+    if len(lst) > 1:
+        raise Exception("an operator is missing between two expressions")
+    return lst[0]
